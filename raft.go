4cb0e0fef552344a15adefff2852c53bc46e651f
Drew Bailey <drew.bailey@stitchfix.com>
2019-07-24 20:10:19 -0400

    fix up a few things, missed bits

--- raft.go
+++ raft.go
@@ -15,7 +15,7 @@ const (
 )
 
 var (
-	keyCurrentTermm = []byte("CurrentTerm")
+	keyCurrentTerm  = []byte("CurrentTerm")
 	keyLastVoteTerm = []byte("LastVoteTerm")
 	keyLastVoteCand = []byte("LastVoteCand")
 	keyCandidateId  = []byte("CandidateId")
@@ -28,22 +28,22 @@ type Raft struct {
 	// Current state
 	state RaftState
 
-	// stable is a StableStore implementation
+	// stable is a StableStore implementation for durable state
 	stable StableStore
 
-	// cache the current term, write through stablestore
+	// Cache the current term, write through to StableStore
 	currentTerm uint64
 
-	// logs is a LogStore implemeation to keep our logs
+	// logs is a LogStore implementation to keep our logs
 	logs LogStore
 
 	// Cache the latest log, though we can get from LogStore
 	lastLog uint64
 
-	// Highest committed log entry
+	// Highest commited log entry
 	commitIndex uint64
 
-	// last applied log to the FSM
+	// Last applied log to the FSM
 	lastApplied uint64
 
 	// FSM is the state machine that can handle the logs
@@ -52,7 +52,7 @@ type Raft struct {
 	// The transport layer we use
 	trans Transport
 
-	// if leader, extra state
+	// If we are the leader, we have extra state
 	leader *LeaderState
 
 	// Shutdown channel to exit
@@ -60,18 +60,15 @@ type Raft struct {
 	shutdownLock sync.Mutex
 }
 
-func NewRaft(
-	conf *Config,
-	stable StableStore,
-	logs LogStore,
-	fsm FSM,
-	trans Transport) (*Raft, error) {
-
+// NewRaft is used to construct a new Raft node
+func NewRaft(conf *Config, stable StableStore, logs LogStore, fsm FSM, trans Transport) (*Raft, error) {
+	// Read the last log value
 	lastLog, err := logs.LastIndex()
 	if err != nil {
-		return nil, fmt.Errorf("failed to find last log: %v", err)
+		return nil, fmt.Errorf("Failed to find last log: %v", err)
 	}
 
+	// Create Raft struct
 	r := &Raft{
 		conf:        conf,
 		state:       Follower,
@@ -85,10 +82,12 @@ func NewRaft(
 		shutdownCh:  make(chan struct{}),
 	}
 
+	// Start the background work
 	go r.run()
 	return r, nil
 }
 
+// run is a long running goroutine that runs the Raft FSM
 func (r *Raft) run() {
 	for {
 		// Check if we are doing a shutdown
@@ -98,6 +97,7 @@ func (r *Raft) run() {
 		default:
 		}
 
+		// Enter into a sub-FSM
 		switch r.state {
 		case Follower:
 			r.runFollower()
@@ -109,54 +109,64 @@ func (r *Raft) run() {
 	}
 }
 
+// runFollower runs the FSM for a follower
 func (r *Raft) runFollower() {
 	ch := r.trans.Consumer()
 	for {
 		select {
 		case rpc := <-ch:
-			// Handle the command
 			switch cmd := rpc.Command.(type) {
 			case *AppendEntriesRequest:
 				r.appendEntries(rpc, cmd)
 			case *RequestVoteRequest:
 				r.requestVote(rpc, cmd)
 			default:
-				log.Printf("[ERR] in Follower state, got unexpected command: %#v", rpc.Command)
+				log.Printf("[ERR] Follower state, got unexpected command: %#v",
+					rpc.Command)
 				rpc.Respond(nil, fmt.Errorf("Unexpected command"))
 			}
+
 		case <-randomTimeout(r.conf.HeartbeatTimeout):
-			// Hearbeat failed! Go to the candidate sttate
+			// Heartbeat failed! Go to the candidate state
 			r.state = Candidate
 			return
+
 		case <-r.shutdownCh:
 			return
 		}
 	}
 }
 
+// runCandidate runs the FSM for a candidate
 func (r *Raft) runCandidate() {
 	ch := r.trans.Consumer()
-	for {
+	transition := false
+	for !transition {
 		select {
 		case rpc := <-ch:
-			// Handle the command
-			switch rpc.Command.(type) {
+			switch cmd := rpc.Command.(type) {
+			case *AppendEntriesRequest:
+				transition = r.appendEntries(rpc, cmd)
+			case *RequestVoteRequest:
+				transition = r.requestVote(rpc, cmd)
 			default:
 				log.Printf("[ERR] Candidate state, got unexpected command: %#v",
 					rpc.Command)
 				rpc.Respond(nil, fmt.Errorf("Unexpected command"))
-
 			}
+
 		case <-randomTimeout(r.conf.ElectionTimeout):
 			// Election failed! Restart the elction. We simply return,
 			// which will kick us back into runCandidate
 			return
+
 		case <-r.shutdownCh:
 			return
 		}
 	}
 }
 
+// runLeader runs the FSM for a leader
 func (r *Raft) runLeader() {
 	for {
 		select {
@@ -166,6 +176,8 @@ func (r *Raft) runLeader() {
 	}
 }
 
+// Shutdown is used to stop the Raft background routines.
+// This is not a graceful operation.
 func (r *Raft) Shutdown() {
 	r.shutdownLock.Lock()
 	defer r.shutdownLock.Lock()
@@ -176,13 +188,14 @@ func (r *Raft) Shutdown() {
 	}
 }
 
+// appendEntries is invoked when we get an append entries RPC call
+// Returns true if we transition to a Follower
 func (r *Raft) appendEntries(rpc RPC, a *AppendEntriesRequest) (transition bool) {
 	// Setup a response
 	resp := &AppendEntriesResponse{
 		Term:    r.currentTerm,
 		Success: false,
 	}
-
 	var err error
 	defer rpc.Respond(resp, err)
 
@@ -191,8 +204,9 @@ func (r *Raft) appendEntries(rpc RPC, a *AppendEntriesRequest) (transition bool)
 		return
 	}
 
-	// Increase the term if we see a newer one
-	if a.Term > r.currentTerm {
+	// Increase the term if we see a newer one, also transition to follower
+	// if we ever get an appendEntries call
+	if a.Term > r.currentTerm || r.state != Follower {
 		r.currentTerm = a.Term
 		resp.Term = a.Term
 
@@ -208,13 +222,13 @@ func (r *Raft) appendEntries(rpc RPC, a *AppendEntriesRequest) (transition bool)
 			a.PrevLogEntry, err)
 		return
 	}
-
 	if a.PrevLogTerm != prevLog.Term {
 		log.Printf("[WARN] Previous log term mis-match: ours: %d remote: %d",
 			prevLog.Term, a.PrevLogTerm)
 		return
 	}
 
+	// Add all the entries
 	for _, entry := range a.Entries {
 		// Delete any conflicting entries
 		if entry.Index <= r.lastLog {
@@ -227,11 +241,11 @@ func (r *Raft) appendEntries(rpc RPC, a *AppendEntriesRequest) (transition bool)
 
 		// Append the entry
 		if err := r.logs.StoreLog(entry); err != nil {
-			log.Printf("[ERR] Failed to append log: %v", err)
+			log.Printf("[ERR] Failed to append to log: %v", err)
 			return
 		}
 
-		// Update the lastlog
+		// Update the lastLog
 		r.lastLog = entry.Index
 	}
 
@@ -239,9 +253,10 @@ func (r *Raft) appendEntries(rpc RPC, a *AppendEntriesRequest) (transition bool)
 	if a.LeaderCommitIndex > r.commitIndex {
 		r.commitIndex = min(a.LeaderCommitIndex, r.lastLog)
 
-		// trigger applying logs locally
+		// TODO: Trigger applying logs locally!
 	}
 
+	// Everything went well, set success
 	resp.Success = true
 	return
 }
